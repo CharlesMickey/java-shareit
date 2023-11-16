40a41ed31e711b1941986af60c29d317cf36b4ab
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-11-17 01:59:54 +0300

    Ref: исправления после проверки встроенным пред ревьюером

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -8,4 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "/bookings")
-public class BookingController {}
+public class BookingController {
+
+}
