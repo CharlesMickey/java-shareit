6499b04a98b5bb6e4120b149079806659479f0db
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-12-25 17:00:11 +0300

    Ref: правка для автотестов.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -18,28 +18,22 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank(message = "Name не может быть пустым")
     @Column
     private String name;
-
     @NotBlank(message = "Description не может быть пустым")
     @Column
-
     private String description;
 
     @NotNull(message = "Available не может быть пустым")
     @Column(name = "is_available")
     private Boolean available;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User owner;
-
     @ManyToOne
     @JoinColumn(name = "request_id")
     private ItemRequest request;
