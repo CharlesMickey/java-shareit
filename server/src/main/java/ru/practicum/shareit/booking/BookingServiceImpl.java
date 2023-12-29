08f1881c1a9d14655d816aee41bcab03e1244ba0
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-12-29 11:04:41 +0300

    Ref: правка для автотестов.

--- server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
